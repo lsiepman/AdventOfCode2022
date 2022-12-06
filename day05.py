17d5557d14f2dfabd95f028d095b39d0000b13c0
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2022-12-06 22:11:05 +0100

    finished day 5

--- day05.py
+++ day05.py
@@ -1,4 +1,7 @@
 import itertools
+from collections import defaultdict
+from copy import deepcopy
+
 # read data
 moves = {}
 stacks = []
@@ -16,3 +19,42 @@ with open("./input/day05.txt") as f:
 
 # transpose stacks
 containers = list(map(list, itertools.zip_longest(*stacks[::-1], fillvalue=None)))
+boxes = defaultdict(list)
+for i in containers:
+    idx = i.pop(0)
+    j = [k.strip() for k in i if k.strip() != ""]
+    boxes[idx.strip()] = j
+
+boxes_part2 = deepcopy(boxes)
+
+# follow instructions part 1
+for i in moves:
+    num_boxes = int(moves[i]["number_to_move"])
+    fro = moves[i]["from"]
+    to = moves[i]["to"]
+    for n in range(num_boxes):
+        boxes[to].append(boxes[fro].pop())
+
+top = []
+for key in boxes:
+    top.append(boxes[key][-1])
+
+print(f"part 1: {''.join(top).replace('[', '').replace(']', '')}")
+
+# follow instructions part 2
+for i in moves:
+    num_boxes = int(moves[i]["number_to_move"])
+    fro = moves[i]["from"]
+    to = moves[i]["to"]
+    temp = []
+    for n in range(num_boxes):
+        temp.append(boxes_part2[fro].pop())
+    temp.reverse()
+    boxes_part2[to] = boxes_part2[to] + temp
+    
+top = []
+for key in boxes_part2:
+    top.append(boxes_part2[key][-1])
+
+print(f"part 2: {''.join(top).replace('[', '').replace(']', '')}")
+
