c8510bd25a12579f1d7fe3857a220b49e9e4bab0
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2022-12-07 00:05:33 +0100

    Improved day6 code

--- day06.py
+++ day06.py
@@ -1,16 +1,11 @@
 # read data
 with open("./input/day06.txt") as file:
     data = file.read().replace("\n", "")
-    data = [i for i in data]
 
 def find_marker(slice):
-    temp_set = set(slice)
-    if len(slice) == len(temp_set):
-        return True
-    else:
-        return False
-
-def part1(data, len_slice):
+    return len(slice) == len(set(slice))
+        
+def calc_answer(data, len_slice):
     for i in range(len(data)):
         if i < len_slice - 1:
             continue
@@ -20,5 +15,5 @@ def part1(data, len_slice):
         if (find_marker(data[slice_start:slice_end])):
             return i + 1
 
-print("Part 1:", part1(data, 4))
-print("Part 2:", part1(data, 14))
+print("Part 1:", calc_answer(data, 4))
+print("Part 2:", calc_answer(data, 14))
